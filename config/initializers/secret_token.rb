00df2ecf84ddc27217dbebe8befb155b4fe57116
crinicom <cristian.menajovsky@gmail.com>
2014-07-10 20:53:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Proto1::Application.config.secret_key_base = 'f54236a80d338c23329821d22d9f1af9c750b4f1b43990cd9aca97845ee4bb5275319e6d619fdf48bafef327b408ab913c5c9744d7f3b01e023d136c50c45b23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Proto1::Application.config.session_store :cookie_store, key: '_proto1_session'
